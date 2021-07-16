c86b77379e73add310e08ff4a31e891f24bf5fd2
Dylan Leonard <dleonard@csumb.edu>
2021-07-16 14:35:38 -0700

    Fixed list not showing up

--- app/src/main/java/com/example/restaurantgenerator/MainActivity.java
+++ app/src/main/java/com/example/restaurantgenerator/MainActivity.java
@@ -1,29 +1,30 @@
-package com.example.restaurantgenerator;
 
-import androidx.appcompat.app.AppCompatActivity;
+        package com.example.restaurantgenerator;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
-import android.widget.Toast;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.LinearLayout;
+        import android.widget.ScrollView;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+        import com.android.volley.Request;
+        import com.android.volley.RequestQueue;
+        import com.android.volley.Response;
+        import com.android.volley.VolleyError;
+        import com.android.volley.toolbox.StringRequest;
+        import com.android.volley.toolbox.Volley;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+        import org.json.JSONArray;
+        import org.json.JSONException;
+        import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private RestaurantDb db;    // create a db, so users can add their restaurants into their list
 
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     LinearLayout restaurantLinearLayout;
     TextView restaurantTextView;
     Button addBtn;
+    Button listedGoBack;
     Button readyBtn;
 
     @Override
@@ -38,27 +40,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         // creates an instance of the database when the app launches
         db = RestaurantDb.getInstance(this);
 
-
-        // click listener for go back button in listed restaurants
-        View listedGoBack = findViewById(R.id.listed_gobackBtn);
-        listedGoBack.setOnClickListener(this); // unsure
+        listedGoBack = (Button)findViewById(R.id.listed_gobackBtn);
+        listedGoBack.setOnClickListener(this);
 
         mainLinearLayout = (LinearLayout)findViewById(R.id.mainLinearLayout);
         readyBtn = (Button)findViewById(R.id.readyBtn);
-
+        readyBtn.setOnClickListener(this);
 
 
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=&location=36.66717694044335,-121.65614460655894&radius=16000&type=restaurant";
+        String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDpj-s6hA4_FqJCryNiA53N7ewA8eufSKw&location=36.66717694044335,-121.65614460655894&radius=16000&type=restaurant";
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-
+                mainLinearLayout = (LinearLayout)findViewById(R.id.mainLinearLayout);
                 try {
                     JSONObject jsonObject = new JSONObject(response);
                     JSONArray restaurants = (JSONArray)jsonObject.get("results");
@@ -80,6 +79,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         addBtn.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
+
                                 // adds the chosen restaurant into the database
                                 if(db.chosenlist().findRestaurantByAddress(restaurantVicinity)){
                                     Toast.makeText(getApplicationContext(), "The restaurant " + restaurantName + " at " + restaurantVicinity +
@@ -88,6 +88,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                     Toast.makeText(getApplicationContext(), restaurantName + " is added to your list", Toast.LENGTH_SHORT).show();
                                     db.chosenlist().addRestaurant(new chosenList(restaurantName, restaurantVicinity));
                                 }
+
                             }
                         });
                         mainLinearLayout.addView(restaurantLinearLayout);
@@ -104,24 +105,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
 
         queue.add(stringRequest);
-
-        readyBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Ready clicked", Toast.LENGTH_SHORT).show();
-                Intent userPicks = new Intent(MainActivity.this, UserPicks.class);
-                startActivity(userPicks);
-            }
-        });
     }
 
+    @Override
     public void onClick(View v) {
         if(v.getId() == R.id.listed_gobackBtn) {
             // button will direct user to cover page(temporarily until categories is set up)
             Intent i = new Intent(this, CoverPage.class);
             startActivity(i);
+        }else if(v.getId() == R.id.readyBtn){
+            Toast.makeText(getApplicationContext(), "Ready clicked", Toast.LENGTH_SHORT).show();
+            Intent userPicks = new Intent(MainActivity.this, UserPicks.class);
+            startActivity(userPicks);
         }
     }
-
-    }
-
+}
