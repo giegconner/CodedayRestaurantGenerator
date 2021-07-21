2a5184284de4156ce0837f7a35e23e21ca499c66
Dylan Leonard <dleonard@csumb.edu>
2021-07-20 22:53:17 -0700

    Testing deletion of restaurants

--- app/src/main/java/com/example/restaurantgenerator/choseListDao.java
+++ app/src/main/java/com/example/restaurantgenerator/choseListDao.java
@@ -1,6 +1,7 @@
 package com.example.restaurantgenerator;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 
@@ -12,14 +13,20 @@ public interface choseListDao {
     @Query("select count(*) from chosenRestaurants")
     int count();
 
+
     @Query("select * from chosenRestaurants where address = :restaurantaddress")
     boolean findRestaurantByAddress(String restaurantaddress);
 
+    // Will return all restaurants the user picks
     @Query("select * from chosenRestaurants")
     List<chosenList> getAllRestaurants();
 
     @Insert
     long[] insertRestaurant(chosenList... chosenLists);
+
+    // Will delete any restaurant with the inputted name (JUST FOR TESTING PURPOSES)
+    @Query("DELETE from chosenRestaurants where restaurant_name = :restaurantName")
+    void deleteByName(String restaurantName);
 
     @Insert
     void addRestaurant(chosenList chosenlist);
